a75bab20cdbd390ff4bd8f73b38908eac2e3e9a9
Prabh Singh <prabhm512@gmail.com>
2020-10-08 00:38:32 +1100

    sliding gallery added to portfolio page

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+// Initialize Swiper
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
