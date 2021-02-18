87428575eef2ad406ca2a5fdfa7ec757b7216666
Prabh Singh <prabhm512@gmail.com>
2021-02-18 17:27:14 +1100

    minor styling changes

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -21,7 +21,7 @@ function ProjectList(props) {
         }>
         <li className={props.data.className}>
             <div className="container">
-                <img src={props.data.image} alt={props.data.alt} className="image" height="260"/>
+                <img src={props.data.image} alt={props.data.alt} className="image" height="250"/>
                 <div className="centered">
                     <div className="wrapper">
                         <div className="link_wrapper">
